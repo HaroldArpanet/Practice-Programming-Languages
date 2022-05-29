c45a8c2b1f6a335444927d7030db52a1c6181592
Harold <harold.arpanet@gmail.com>
2022-05-29 14:26:03 +0430

    Create main.java

---
+++ JAVA/S-SH/PART3/main.java
@@ -0,0 +1,41 @@
+public class main
+{
+    public static void main(String[] arg){
+        /* EXAMPLEe FOR VARIABLE */
+        int x = 123;
+        long y = 143437589891483104L;
+        double d = 23474.634839;
+        /* EXAMPLE FOR ALL OF MAX AND MIN IN TYPE OF VARIABLE */
+        byte maxByte = Byte.MAX_VALUE;
+        byte minByte = Byte.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        short minShort = Short.MIN_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+        long maxLong = Long.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE;
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+        /* EXAMPLE Of CHAR */
+        char c = 'A';
+        /* Print ALL OF THEM */
+        System.out.println("PRINT INT X : "+x);
+        System.out.println("PRINT LONG Y : "+y);
+        System.out.println("PRINT DOUBLE D : "+d);
+        System.out.println("PRINT MAX BYTE : "+maxByte);
+        System.out.println("PRINT MIN BYTE : "+minByte);
+        System.out.println("PRINT MAX SHORT : "+maxShort);
+        System.out.println("PRINT MIN SHORT : "+minShort);
+        System.out.println("PRINT MAX INT : "+maxInt);
+        System.out.println("PRINT MIN INT : "+minInt);
+        System.out.println("PRINT MAX LONG : "+maxLong);
+        System.out.println("PRINT MIN LONG : "+minLong);
+        System.out.println("PRINT MAX FLOAT : "+maxFloat);
+        System.out.println("PRINT MIN FLOAT : "+minFloat);
+        System.out.println("PRINT MAX DOUBLE : "+maxDouble);
+        System.out.println("PRINT MIN DOUBLE : "+minDouble);
+        System.out.println("PRINT CHAR : "+c);
+    }
+}
